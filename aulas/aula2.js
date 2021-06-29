baa1faffa4e453b46e1cf795b66915175fd143ff
Lúcio Noleto <54680682+lucioln@users.noreply.github.com>
2021-06-29 13:10:41 -0300

    Add files via upload

---
+++ aulas/aula2.js
@@ -0,0 +1,9 @@
+/*
+Operadores Lógicos
+&&      And -> Todas as expressões precisam ser verdadeiras para retornar true.
+||      OR
+!       NOT
+*/
+//const expressaoAnd = true && true && true && true;
+const expressaoOR = true || false;
+console.log(!10);
---
+++ aulas/aula3.js
@@ -0,0 +1,6 @@
+function falaOi() {
+    return 'Oi';
+}
+
+const vaiExecutar = false;
+console.log(vaiExecutar && falaOi());
